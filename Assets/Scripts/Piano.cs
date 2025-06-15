faa9f204af3989fc69ec4b6f7ca4fc39852463a4
gregsexauer <77690060+gregsexauer@users.noreply.github.com>
2025-06-15 16:20:33 -0400

    you can fix the piano rope

---
+++ Assets/Scripts/Piano.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Yarn.Unity;
+
+public class Piano : MonoBehaviour
+{
+    [SerializeField] Sprite fixedSprite;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    static bool _isFixed = false;
+
+    [YarnCommand("Replace_Rope")]
+    public void FixPiano()
+    {
+        spriteRenderer.sprite = fixedSprite;
+        _isFixed = true;
+    }
+
+    [YarnFunction("Is_Piano_Fixed")]
+    public static bool IsPianoFixed()
+    {
+        return _isFixed;
+    }
+}
