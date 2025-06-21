3935a4f17041bfd29aa6ff12b4162a0c4b318a78
gregsexauer <77690060+gregsexauer@users.noreply.github.com>
2025-06-21 19:04:44 -0400

    new library door

--- Assets/Scripts/LibraryDoor.cs
+++ Assets/Scripts/LibraryDoor.cs
@@ -4,9 +4,13 @@ using DG.Tweening;
 
 public class LibraryDoor : MonoBehaviour
 {
+    [SerializeField] Transform doorLeft;
+    [SerializeField] Transform doorRight;
     [YarnCommand("Open_Door")]
     public void OpenDoor()
     {
-        transform.DORotate(new Vector3(0, -90, 0), .8f);
+        doorLeft.DORotate(new Vector3(0, -90, 0), .8f);
+        doorRight.DORotate(new Vector3(0, 90, 0), .8f);
+        GetComponentInChildren<Collider>().enabled = false;
     }
 }
