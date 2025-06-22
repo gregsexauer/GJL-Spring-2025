84e4fed9c98f13726ca30531ab300140fad305b3
gregsexauer <77690060+gregsexauer@users.noreply.github.com>
2025-06-21 23:06:36 -0400

    scout it petable :)

--- Assets/Scripts/NPCs/Scout.cs
+++ Assets/Scripts/NPCs/Scout.cs
@@ -1,11 +1,27 @@
+using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 using Yarn.Unity;
 
 public class Scout : MonoBehaviour
 {
+    [SerializeField] Transform heart;
+
     [YarnCommand("Pet_Scout")]
     public void PetScout()
     {
-        Debug.Log("pet scout :)");
+        StartCoroutine(Pet());
+    }
+
+    IEnumerator Pet()
+    {
+        GetComponent<DialogueInteractable>().IsActive = false;
+        heart.DOScale(3, .15f).SetEase(Ease.OutBounce);
+        yield return new WaitForSeconds(1);
+        heart.DOScale(0, .25f).SetEase(Ease.OutFlash);
+
+        yield return new WaitForSeconds(.25f);
+
+        GetComponent<DialogueInteractable>().IsActive = true;
     }
 }
